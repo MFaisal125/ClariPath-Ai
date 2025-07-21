0e0fe07f8502e1e1115648146f99d69ec211eff2
MFaisal125 <mfaisal0125470@gmail.com>
2025-07-21 14:27:10 +0500

    Ai Career Agent

--- server/index.js
+++ server/index.js
@@ -7,33 +7,40 @@ dotenv.config();
 const app = express();
 
 // CORS Configuration
-app.use(cors({
-    origin: ['https://myai-portfolio.vercel.app','http://localhost:3000', 'https://career-craft-ai.vercel.app', 'http://localhost:5173', 'https://ai-career-pilot.vercel.app'],
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type',
+app.use(
+  cors({
+    origin: [
+      "https://ai-coach-career.vercel.app",
+      "http://localhost:3000",
+      "https://clari-path-ai-agent.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type",
     credentials: true,
-}));
+  })
+);
 
 // Middleware
 app.use(express.json());
 
 // Routes
-const UserRouter = require("./routes/User")
-app.use('/user', UserRouter);
+const UserRouter = require("./routes/User");
+app.use("/user", UserRouter);
 
 const PORT = process.env.PORT || 5005;
 
 // Connect to MongoDB and Start Server
 connectMongoDB()
-    .then(() => {
-        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-    })
-    .catch((error) => {
-        console.error("❌ MongoDB Connection Failed:", error);
-        process.exit(1);
-    });
+  .then(() => {
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  });
 
 // Default Route
 app.get("/", (req, res) => {
-    res.send("🚀 Welcome to Career Craft AI Backend");
+  res.send("🚀 Welcome to Career Craft AI Backend");
 });
